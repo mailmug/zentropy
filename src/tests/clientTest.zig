3a1e9472d8c8362526e7436270faeb8af4a2c9c0
mrtowers <dawmc52@gmail.com>
2025-10-20 12:49:13 +0200

    client test file done

--- src/tests/clientTest.zig
+++ src/tests/clientTest.zig
@@ -29,18 +29,46 @@ test "set-get" {
 
     Thread.sleep(time.ns_per_ms * 1000);
 
+    const ex1 = "example1";
+    const ex2 = "example 2"; //with spaces
+    const val1 = "value1";
+    const val2 = "value 2";
+
     var client = try zentropy.Client.connect(.{});
 
-    try client.set("example1", "value1");
-    try client.set("example2", "value2");
+    try client.set(ex1, val1);
+    try client.set(ex2, val2);
 
     var value1_buf: [32]u8 = undefined;
     var value2_buf: [32]u8 = undefined;
+    var value3_buf: [32]u8 = undefined;
 
-    const value1 = try client.get("example1", &value1_buf) orelse unreachable;
-    try testing.expectEqualSlices(u8, "value1", value1);
-    const value2 = try client.get("example2", &value2_buf) orelse unreachable;
-    try testing.expectEqualSlices(u8, "value2", value2);
+    // simple .get
+    const value1 = try client.get(ex1, &value1_buf) orelse unreachable;
+    try testing.expectEqualSlices(u8, val1, value1);
+    const value2 = try client.get(ex2, &value2_buf) orelse unreachable;
+    try testing.expectEqualSlices(u8, val2, value2);
+    const value3 = try client.get("not existing", &value3_buf);
+    try testing.expect(value3 == null);
+
+    // .getAlloc
+    const value1_alloc = try client.getAlloc(testing.allocator, ex1) orelse unreachable;
+    defer testing.allocator.free(value1_alloc);
+    try testing.expectEqualSlices(u8, val1, value1_alloc);
+    const value2_alloc = try client.getAlloc(testing.allocator, ex2) orelse unreachable;
+    defer testing.allocator.free(value2_alloc);
+    try testing.expectEqualSlices(u8, val2, value2_alloc);
+    const value3_alloc = try client.getAlloc(testing.allocator, "not existing");
+    try testing.expect(value3_alloc == null);
+
+    // .getSized
+    const value1_sized = try client.getSized(ex1, val1.len) orelse unreachable;
+    try testing.expectEqualSlices(u8, val1, value1_sized);
+    const value2_sized = try client.getSized(ex2, val2.len) orelse unreachable;
+    try testing.expectEqualSlices(u8, val2, value2_sized);
+    const value3_sized = try client.getSized("not existing", 5);
+    try testing.expect(value3_sized == null);
+
     client.shutdown() catch unreachable;
     client.deinit();
 
